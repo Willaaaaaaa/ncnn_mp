e8d43127df3772b459ad7e2eaae89124660f54ce
Willaaaaaaa <willaaaaaaa936@gmail.com>
2025-08-24 22:47:51 +0800

    fix: oversize when building esp32s3 and makefile

--- modules/ncnn_mp/micropython.mk
+++ modules/ncnn_mp/micropython.mk
@@ -4,21 +4,20 @@ NCNN_INSTALL_PREFIX := $(NCNN_DIR)/build/install
 
 SRC_USERMOD_C += $(NCNN_MP_MOD_DIR)/ncnn_mp.c
 
-# # Add OpenMP flag for C and C++ compilers
-# CFLAGS_USERMOD += -fopenmp
-# CXXFLAGS_USERMOD += -fopenmp
-
 # Include directories
-CXXFLAGS_USERMOD += -I$(NCNN_INSTALL_PREFIX)/include
 CFLAGS_USERMOD += -I$(NCNN_INSTALL_PREFIX)/include
 
 # Other compiler flags
-CXXFLAGS_USERMOD += -std=c++11
 CFLAGS_USERMOD += -Wno-unused-function
-CXXFLAGS_USERMOD += -Wno-unused-function
 
 # Linker flags
 LDFLAGS_USERMOD += -L$(NCNN_INSTALL_PREFIX)/lib
-LDFLAGS_USERMOD += -lncnn
+
+ifeq ($(USE_VULKAN), 1)
+    CFLAGS_USERMOD += -DNCNN_VULKAN=ON
+    LDFLAGS_USERMOD += -lncnn -lglslang -lSPIRV -lOSDependent -lGenericCodeGen -lMachineIndependent -lglslang-default-resource-limits -lvulkan
+else
+    LDFLAGS_USERMOD += -lncnn
+endif
+
 LDFLAGS_USERMOD += -lstdc++
-# LDFLAGS_USERMOD += -fopenmp
